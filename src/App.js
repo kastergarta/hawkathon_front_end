8633141ff12b311587ac48ff77116ef4ec2ec833
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-09-19 09:59:50 -0400

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as emailjs from 'emailjs-com'
 
 function App() {
+
+  state = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const { name, email, subject, message } = this.state
+    let templateParams = {
+      from_name: email,
+      to_name: '<YOUR_EMAIL_ID>',
+      subject: subject,
+      message_html: message,
+     }
+     emailjs.send(
+      'gmail',
+      'template_XXXXXXXX',
+       templateParams,
+      'user_XXXXXXXXXXXXXXXXXXXX'
+     )
+     this.resetForm()
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form id="contact-form">
+      <input type="hidden" name="contact_number"/>
+      <label>Name</label>
+      <input type="text" name="user_name"/>
+      <label>Email</label>
+      <input type="email" name="user_email"/>
+      <label>Message</label>
+      <textarea name="message"></textarea>
+      <input type="submit" value="Send"/>
+  </form>
     </div>
   );
 }
